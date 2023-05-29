6cc6ca4a094f16b58d16851ab187809190d1c198
Jeremy Hage <jeremy.hage@gmail.com>
2023-05-29 12:21:22 -0500

    updated

---
+++ .config/nvim/after/plugin/lspsaga.lua
@@ -0,0 +1,2 @@
+local opts = {noremap = true, silent = true}
+vim.keymap.set("n", "K","<cmd>Lspsaga hover_doc<CR>", opts)
--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,23 +1,18 @@
-require('nvim-treesitter.configs').setup({
+require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  -- ensure_installed = { "c", "lua", "rust", "javascript", "json", "vim", "javascript", "css", "dockerfile", "bash"},
-  ensure_installed = "all",
+  ensure_installed = { "help", "javascript", "typescript", "c", "lua", "rust" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
 
-  -- List of parsers to ignore installing (for "all")
-  ignore_install = {  },
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
 
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
-
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    -- disable = { "c", "rust" },
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
@@ -25,4 +20,4 @@ require('nvim-treesitter.configs').setup({
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
-})
+}
