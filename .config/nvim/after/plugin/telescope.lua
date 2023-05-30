d966ca69fdd86b4bae18d04158ed9a128bb402b4
Jeremy Hage <jeremy.hage@gmail.com>
2023-05-30 08:45:23 -0500

    remove top level .config dir

--- .config/nvim/after/plugin/telescope.lua
+++
@@ -1,8 +0,0 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
-vim.keymap.set('n', '<leader>pb', builtin.grep_string, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({search = vim.fn.input("Grep > ")})
-end) 
-vim.keymap.set('n', '<leader>vpr', builtin.lsp_references)
--- .config/nvim/lua/kimchicoder/init.lua
+++
@@ -1,3 +0,0 @@
-require('kimchicoder.remap')
-require('kimchicoder.packer')
-require('kimchicoder.set')
