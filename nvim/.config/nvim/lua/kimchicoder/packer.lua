1b58dcb5090624b0f8cd535c8fed620f14277364
Jeremy Hage <jeremy.hage@gmail.com>
2023-05-30 14:54:39 -0500

    update key bindings; add lualine

--- nvim/.config/nvim/lua/kimchicoder/packer.lua
+++ nvim/.config/nvim/lua/kimchicoder/packer.lua
@@ -4,88 +4,116 @@
 vim.cmd.packadd("packer.nvim")
 
 return require("packer").startup(function(use)
-	-- Packer can manage itself
-	use("wbthomason/packer.nvim")
-	use("glepnir/lspsaga.nvim")
+    -- Packer can manage itself
+    use("wbthomason/packer.nvim")
+    use("glepnir/lspsaga.nvim")
 
-	use("nvim-tree/nvim-web-devicons")
-	use({
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.0",
-		-- or                            , branch = '0.1.x',
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
+    use("nvim-tree/nvim-web-devicons")
+    use({
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.0",
+        -- or                            , branch = '0.1.x',
+        requires = { { "nvim-lua/plenary.nvim" } },
+    })
 
-	-- Color Schemes
-	use("folke/tokyonight.nvim")
-	use("rebelot/kanagawa.nvim")
+    -- Color Schemes
+    use("folke/tokyonight.nvim")
+    use("rebelot/kanagawa.nvim")
 
-	use({ "catppuccin/nvim", as = "catppuccin" })
-	use({
-		"rose-pine/neovim",
-		as = "rose-pine",
-		config = function()
-			vim.cmd("colorscheme rose-pine")
-		end,
-	})
+    use({ "catppuccin/nvim", as = "catppuccin" })
+    use({
+        "rose-pine/neovim",
+        as = "rose-pine",
+        config = function()
+            vim.cmd("colorscheme rose-pine")
+        end,
+    })
 
-	-- Status Line
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "nvim-tree/nvim-web-devicons", opt = true },
-	})
-	-- Comment
-	use({
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup()
-		end,
-	})
-	-- Indent when creating new blankline
-	use("lukas-reineke/indent-blankline.nvim")
+    -- Status Line
+    use({
+        "nvim-lualine/lualine.nvim",
+        requires = { "nvim-tree/nvim-web-devicons", opt = true },
+    })
+    -- Comment
+    use({
+        "numToStr/Comment.nvim",
+        config = function()
+            require("Comment").setup()
+        end,
+    })
+    -- Indent when creating new blankline
+    use("lukas-reineke/indent-blankline.nvim")
 
-	-- Formatting and Linting
-	use("jose-elias-alvarez/null-ls.nvim")
+    -- Formatting and Linting
+    use("jose-elias-alvarez/null-ls.nvim")
 
-	---
-	use({
-		"folke/trouble.nvim",
-		requires = "nvim-tree/nvim-web-devicons",
-		config = function()
-			require("trouble").setup({
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
-			})
-		end,
-	})
-	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
-	use("nvim-treesitter/playground")
-	use("nvim-treesitter/nvim-treesitter-context")
-	use("nvim-lua/plenary.nvim")
-	use("ThePrimeagen/harpoon")
-	use("tpope/vim-fugitive")
-	use("mbbill/undotree")
-	use({
-		"VonHeikemen/lsp-zero.nvim",
-		requires = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" },
-			{ "williamboman/mason.nvim" },
-			{ "williamboman/mason-lspconfig.nvim" },
+    ---
+    use({
+        "folke/trouble.nvim",
+        requires = "nvim-tree/nvim-web-devicons",
+        config = function()
+            require("trouble").setup({
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            })
+        end,
+    })
+    use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+    use("nvim-treesitter/playground")
+    use("nvim-treesitter/nvim-treesitter-context")
+    use("nvim-lua/plenary.nvim")
+    use("ThePrimeagen/harpoon")
+    use("tpope/vim-fugitive")
+    use("mbbill/undotree")
+    use({
+        "VonHeikemen/lsp-zero.nvim",
+        requires = {
+            -- LSP Support
+            { "neovim/nvim-lspconfig" },
+            { "williamboman/mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" },
 
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" },
-			{ "hrsh7th/cmp-buffer" },
-			{ "hrsh7th/cmp-path" },
-			{ "saadparwaiz1/cmp_luasnip" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "hrsh7th/cmp-nvim-lua" },
+            -- Autocompletion
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            { "saadparwaiz1/cmp_luasnip" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-nvim-lua" },
 
-			-- Snippets
-			{ "L3MON4D3/LuaSnip" },
-			{ "rafamadriz/friendly-snippets" },
-		},
-	})
-	use("kdheepak/lazygit.nvim")
+            -- Snippets
+            { "L3MON4D3/LuaSnip" },
+            { "rafamadriz/friendly-snippets" },
+        },
+    })
+    use("kdheepak/lazygit.nvim")
+    use {
+        'kdheepak/tabline.nvim',
+        config = function()
+            require 'tabline'.setup {
+                -- Defaults configuration options
+                enable = true,
+                options = {
+                    -- If lualine is installed tabline will use separators configured in lualine by default.
+                    -- These options can be used to override those settings.
+                    section_separators = { '', '' },
+                    component_separators = { '', '' },
+                    max_bufferline_percent = 66, -- set to nil by default, and it uses vim.o.columns * 2/3
+                    show_tabs_always = false, -- this shows tabs only when there are more than one tab or if the first tab is named
+                    show_devicons = true, -- this shows devicons in buffer section
+                    show_bufnr = false, -- this appends [bufnr] to buffer section,
+                    show_filename_only = false, -- shows base filename only instead of relative path in filename
+                    modified_icon = "+ ", -- change the default modified icon
+                    modified_italic = false, -- set to true by default; this determines whether the filename turns italic if modified
+                    show_tabs_only = false, -- this shows only tabs instead of tabs + buffers
+                }
+            }
+            vim.cmd [[
+      set guioptions-=e " Use showtabline in gui vim
+      set sessionoptions+=tabpages,globals " store tabpages and globals in session
+    ]]
+        end,
+        requires = { { 'hoob3rt/lualine.nvim', opt = true }, { 'kyazdani42/nvim-web-devicons', opt = true } }
+    }
 end)
