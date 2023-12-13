ca7772f3d81a1422680cfc7a18cf6e463f7fb574
Jeremy Hage <jeremy.hage@gmail.com>
2023-12-13 11:49:40 -0600

    use lazyvim; update zellij config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,2 @@
-require('kimchicoder')
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
---
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
+  },
+}
