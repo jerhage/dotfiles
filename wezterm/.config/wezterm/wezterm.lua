d08cb0ee859a0f0d08adf3c54d696eab98bcac09
Jeremy Hage <jeremy.hage@gmail.com>
2024-01-06 08:26:14 -0600

    add systemd service; move starship config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,11 +6,13 @@ if wezterm.config_builder then
 end
 
 config.enable_scroll_bar = true
-config.font = wezterm.font("FiraMono Nerd Font")
--- config.font = wezterm.font("JetBrains Mono Nerd Font")
+config.font = wezterm.font("FiraCode Nerd Font Mono")
+-- config.font = wezterm.font("FiraMono Nerd Font")
+-- config.font = wezterm.font("JetBrains Mono")
 config.window_decorations = "NONE"
 config.enable_tab_bar = false
 config.window_background_opacity = 0.7
 config.warn_about_missing_glyphs = false
-
+config.default_prog = { "/bin/bash" }
+-- config.default_prog = { "/bin/bash", "-l" }
 return config
