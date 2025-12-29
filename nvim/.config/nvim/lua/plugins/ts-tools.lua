0eae312bc608102b8cde2b23803243e03d8291f6
Jeremy Hage <jhage.codingkr@gmail.com>
2025-12-29 10:26:12 -0600

    add mpv; configure ssh

--- nvim/.config/nvim/lua/plugins/ts-tools.lua
+++ nvim/.config/nvim/lua/plugins/ts-tools.lua
@@ -1,5 +1,6 @@
 return {
 	"pmizio/typescript-tools.nvim",
+	enabled = false,
 	requires = { "nvim-lua/plenary", "neovim/nvim-lspconfig" },
 	config = function()
 		require("typescript-tools").setup({})
